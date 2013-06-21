a8dcc1e6196039aeaac188b04186adb4ce2afa76
crynobone <crynobone@gmail.com>
2013-06-21 12:01:09 +0800

    Add placeholder on configuring orchestra/story.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/orchestra.php
+++ src/orchestra.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\View;
 use Orchestra\Support\Facades\Acl;
 use Orchestra\Support\Facades\App;
 use Orchestra\Support\Facades\Config;
+use Orchestra\Support\Facades\Widget;
 
 Acl::make('orchestra/story')->attach(App::memory());
 
@@ -16,6 +18,12 @@ Config::map('orchestra/story', array(
 
 Event::listen('orchestra.ready: admin', 'Orchestra\Story\Services\Events\DashboardHandler@onDashboardView');
 Event::listen('orchestra.form: extension.orchestra/story', 'Orchestra\Story\Services\Events\ExtensionHandler@onFormView');
+Event::listen('orchestra.form: extension.orchestra/story', function ()
+{
+	$placeholder = Widget::make('placeholder.orchestra.extensions');
+	$placeholder->add('permalink')->value(View::make('orchestra/story::widgets.help'));
+});
+
 Event::listen('orchestra.validate: extension.orchestra/story', function (& $rules)
 {
 	$rules['page_permalink'] = array('required');
---
+++ src/views/widgets/help.php
@@ -0,0 +1,14 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		List of Available Permalink
+	</div>
+
+	<ul class="">
+		<li><code>{slug}</code></li>
+		<li><code>{id}</code></li>
+	</ul>
+
+	<div class="panel-footer">
+		Each permalink should at least have either <code>{slug}</code> or <code>{id}</code>.
+	</div>
+</div>
