ab4127210257a8b80e10f68993c449270171d120
crynobone <crynobone@gmail.com>
2013-07-04 11:17:39 +0800

    Tweak widget markup
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/widgets/help.php
+++ src/views/widgets/help.php
@@ -1,14 +1,18 @@
-<div class="panel panel-warning">
+<div class="panel panel-warning widget">
 	<div class="panel-heading">
 		List of Available Permalink
 	</div>
-
-	<ul class="">
-		<li><code>{slug}</code></li>
-		<li><code>{id}</code></li>
-	</ul>
-
-	<div class="panel-footer">
-		Each permalink should at least have either <code>{slug}</code> or <code>{id}</code>.
+	<div class="box white rounded-top">
+		<p>
+			Each permalink should at least have either <code>{slug}</code> or <code>{id}</code>.
+		</p>
+	</div>
+	<div class="box white no-padding rounded-bottom list-group">
+		<a href="#" class="list-group-item"><code>{slug}</code></a>
+		<a href="#" class="list-group-item"><code>{id}</code></a>
+		<a href="#" class="list-group-item"><code>{year}</code></a>
+		<a href="#" class="list-group-item"><code>{month}</code></a>
+		<a href="#" class="list-group-item"><code>{day}</code></a>
+		<a href="#" class="list-group-item"><code>{date}</code></a>
 	</div>
 </div>
