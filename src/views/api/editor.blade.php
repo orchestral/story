3151f173eece8616265e3de5bb9161da02f20110
crynobone <crynobone@gmail.com>
2013-07-06 02:30:59 +0800

    Working on editor page.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/api/editor.blade.php
+++ src/views/api/editor.blade.php
@@ -1,3 +1,22 @@
-@include('orchestra/foundation::layout.widgets.header')
+<?php echo Form::open(array('url' => resources('storycms.posts'), 'method' => 'post', 'class' => 'form-horizontal')); ?>
+	<div class="row">
+		<label class="col-lg-2 control-label" for="title">Title</label>
+		<div class="col-lg-10">
+			<?php echo Form::text('title', Input::old('title')); ?>
+		</div>
+	</div>
 
-<?php echo Form::textarea('content'); ?>
+	<div class="row">
+		<label class="col-lg-2 control-label" for="slug">Slug</label>
+		<div class="col-lg-10">
+			<?php echo Form::text('slug', Input::old('slug')); ?>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col col-lg-12">
+			<?php echo Form::textarea('content'); ?>
+		</div>
+	</div>
+
+<?php echo Form::close(); ?>
