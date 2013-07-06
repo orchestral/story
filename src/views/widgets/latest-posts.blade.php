1e2d6d81c3f35b514638a676c2f04a6301993050
crynobone <crynobone@gmail.com>
2013-07-06 13:53:49 +0800

    Fixed url.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/widgets/latest-posts.blade.php
+++ src/views/widgets/latest-posts.blade.php
@@ -1,5 +1,5 @@
 <div class="box white rounded list-group no-padding">
 	<?php foreach ($posts as $post) : ?>
-	<a href="#" class="list-group-item"><?php echo $post->title; ?></a>
+	<a href="<?php echo $post->link; ?>" class="list-group-item"><?php echo $post->title; ?></a>
 	<?php endforeach; ?>
 </div>
