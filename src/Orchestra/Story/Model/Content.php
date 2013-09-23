ee5853caa32205348138243df8ccdd6f7cb81391
crynobone <crynobone@gmail.com>
2013-09-23 21:00:44 +0800

    Update to use attributes mutator property for date instead overloading getDates(), and update some code.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Story/Model/Content.php
+++ src/Orchestra/Story/Model/Content.php
@@ -33,14 +33,11 @@ class Content extends Eloquent {
 	const PUBLISHED_AT = 'published_at';
 
 	/**
-	 * Get the attributes that should be converted to dates.
+	 * The attributes that should be mutated to dates.
 	 *
-	 * @return array
+	 * @var array
 	 */
-	public function getDates()
-	{
-		return array(static::CREATED_AT, static::UPDATED_AT, static::DELETED_AT, static::PUBLISHED_AT);
-	}
+	protected $dates = array('published_at');
 
 	/**
 	 * Belongs to relationship with User.
@@ -95,12 +92,12 @@ class Content extends Eloquent {
 	 *
 	 * @return void
 	 */
-	public function scopeLatestBy($query, $by = null, $take = null)
+	public function scopeLatestBy($query, $orderBy = null, $take = null)
 	{
-		if (is_null($by)) $by = static::PUBLISHED_AT;
+		if (is_null($orderBy)) $orderBy = static::PUBLISHED_AT;
 		if (is_int($take) and $take > 0) $query->take($take);
 
-		$query->orderBy($by, 'DESC');
+		$query->orderBy($orderBy, 'DESC');
 	}
 
 	/**
@@ -141,6 +138,7 @@ class Content extends Eloquent {
 	/**
 	 * Accessor for link.
 	 *
+	 * @param  mixed   $value
 	 * @return void
 	 */
 	public function getLinkAttribute($value)
