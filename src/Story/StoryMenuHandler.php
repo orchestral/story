bc003f8b483dc267b39f4633de9006c725484f34
crynobone <crynobone@gmail.com>
2014-12-01 08:13:36 +0800

    Create Orchestra\Story\StoryMenuHandler. Closes #48
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Story/StoryMenuHandler.php
+++ src/Story/StoryMenuHandler.php
@@ -1,9 +1,40 @@
 <?php namespace Orchestra\Story;
 
+use Orchestra\Contracts\Foundation\Foundation;
+
 class StoryMenuHandler
 {
+    /**
+     * ACL instance.
+     *
+     * @var \Orchestra\Contracts\Authorization\Authorization
+     */
+    protected $acl;
+    /**
+     * Menu instance.
+     *
+     * @var \Orchestra\Widget\Handlers\Menu
+     */
+    protected $menu;
+    /**
+     * Construct a new handler.
+     *
+     * @param  \Orchestra\Contracts\Foundation\Foundation  $foundation
+     */
+    public function __construct(Foundation $foundation)
+    {
+        $this->menu = $foundation->menu();
+        $this->acl = $foundation->acl();
+    }
+    /**
+     * Create a handler for `orchestra.ready: admin` event.
+     *
+     * @return void
+     */
     public function handle()
     {
-
+        $this->menu->add('storycms', '^:extensions')
+            ->title('Story CMS')
+            ->link(handles('orchestra::storycms'));
     }
 }
