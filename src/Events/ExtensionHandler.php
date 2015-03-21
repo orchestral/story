84b11a5b2b6f8b52cd06ade421cd1c481cce6ded
crynobone <crynobone@gmail.com>
2015-03-22 02:09:32 +0800

    Simplify extension handler.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Events/ExtensionHandler.php
+++ src/Events/ExtensionHandler.php
@@ -17,19 +17,18 @@ class ExtensionHandler
     {
         $form->extend(function ($form) use ($model) {
             $form->fieldset('Page Management', function ($fieldset) {
-                $fieldset->control('select', 'default_format', function ($control) {
-                    $control->label('Default Format');
-                    $control->options(StoryFormat::getParsers());
-                });
+                $pages = array_merge(
+                    ['_posts_' => 'Display Posts'],
+                    Content::page()->publish()->lists('title', 'slug')
+                );
 
-                $fieldset->control('select', 'default_page', function ($control) {
-                    $pages = array_merge(
-                        ['_posts_' => 'Display Posts'],
-                        Content::page()->publish()->lists('title', 'slug')
-                    );
-                    $control->label('Default Page');
-                    $control->options($pages);
-                });
+                $fieldset->control('select', 'default_format')
+                    ->label('Default Format')
+                    ->options(StoryFormat::getParsers());
+
+                $fieldset->control('select', 'default_page')
+                    ->label('Default Page')
+                    ->options($pages);
 
                 $fieldset->control('text', 'Page Permalink', 'page_permalink');
                 $fieldset->control('text', 'Post Permalink', 'post_permalink');
