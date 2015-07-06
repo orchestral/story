fed65d05b6ce8c10356c4100e0ee880b618f4df2
crynobone <crynobone@gmail.com>
2015-07-06 08:03:32 +0800

    Tweak editor assets.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Listeners/AttachMarkdownEditor.php
+++ src/Listeners/AttachMarkdownEditor.php
@@ -1,4 +1,4 @@
-<?php namespace Orchestra\Story\Listeners; 
+<?php namespace Orchestra\Story\Listeners;
 
 use Orchestra\Asset\Factory;
 
@@ -31,8 +31,9 @@ class AttachMarkdownEditor
         $asset = $this->asset->container('orchestra/foundation::footer');
 
         $asset->script('editor', 'packages/orchestra/story/vendor/editor/editor.js');
+        $asset->script('marked', 'packages/orchestra/story/vendor/editor/marked.js', ['editor']);
         $asset->style('editor', 'packages/orchestra/story/vendor/editor/editor.css');
         $asset->script('storycms', 'packages/orchestra/story/js/storycms.min.js');
-        $asset->script('storycms.md', 'packages/orchestra/story/js/markdown.min.js', ['editor']);
+        $asset->script('storycms.md', 'packages/orchestra/story/js/markdown.min.js', ['editor', 'marked']);
     }
 }
