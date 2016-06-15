b902d59ca167827ab803341b6d4b794109e11816
crynobone <crynobone@gmail.com>
2016-06-15 23:19:50 +0800

    Update editor.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/admin/editor.blade.php
+++ resources/views/admin/editor.blade.php
@@ -8,7 +8,7 @@
     <div class="form-group{!! $errors->has('title') ? ' has-error': '' !!}">
       <label class="col-md-2 control-label" for="title">Title</label>
       <div class="col-md-10">
-        {{ Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'v-model' => 'title']) }}
+        {{ Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'v-model' => 'content.title', '@keyup' => 'updateFromTitle']) }}
         {!! $errors->first('title', '<p class="help-block error">:message</p>') !!}
       </div>
     </div>
@@ -16,14 +16,14 @@
     <div class="form-group{!! $errors->has('slug') ? ' has-error': ' ' !!}">
       <label class="col-md-2 control-label" for="slug">Slug</label>
       <div class="col-md-10">
-        {{ Form::text('slug', null, ['role' => 'slug-editor', 'class' => 'form-control', 'v-model' => 'slug']) }}
+        {{ Form::text('slug', null, ['class' => 'form-control', 'v-model' => 'slug', '@blur' => 'updateFromSlug']) }}
         {!! $errors->first('slug', '<p class="help-block error">:message</p>') !!}
       </div>
     </div>
 
     <div class="form-group{!! $errors->has('content') ? ' has-error': '' !!}">
       <div class="twelve columns">
-        {{ Form::textarea('content', null, ['class' => 'form-control']) }}
+        {{ Form::textarea('content', null, ['class' => 'form-control', 'id' => 'content']) }}
         {!! $errors->first('content', '<p class="help-block error">:message</p>') !!}
       </div>
     </div>
@@ -39,15 +39,44 @@
     </div>
   </fieldset>
 {{ Form::close() }}
-
 @stop
 
 @push('orchestra.footer')
 <script>
-  var app = new App({
+  var app
+
+  app = new App({
     data: {
+      content: {!! $content->makeVisible('slug')->toJson() !!},
+      sluggable: true,
+      slug: '',
       sidebar: {
         active: 'storycms-write'
+      }
+    },
+
+    ready: function() {
+      this.sluggable = ! this.$get('content.id') > 0
+      this.slug = this.slugify(this.$get('content.slug') || '', '-')
+    },
+
+    methods: {
+      slugify: function(string, separator) {
+        return string.toLowerCase()
+          .replace(/^(_post_\/|_page_\/)/g, '')
+          .replace(/[^\w\.]+/g, separator)
+          .replace(/\s+/g, separator)
+      },
+
+      updateFromTitle: function() {
+        if (this.sluggable) {
+          this.slug = this.slugify(this.content.title, '-')
+        }
+      },
+
+      updateFromSlug: function() {
+        this.sluggable = false
+        this.slug = this.slugify(this.slug, '-')
       }
     }
   }).$mount('body')
