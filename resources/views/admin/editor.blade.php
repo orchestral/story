65b3af29b18f3d20faee50695e306fd98ac6b22d
crynobone <crynobone@gmail.com>
2016-06-15 23:40:44 +0800

    Update views.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/admin/editor.blade.php
+++ resources/views/admin/editor.blade.php
@@ -8,7 +8,7 @@
     <div class="form-group{!! $errors->has('title') ? ' has-error': '' !!}">
       <label class="col-md-2 control-label" for="title">Title</label>
       <div class="col-md-10">
-        {{ Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'v-model' => 'content.title', '@keyup' => 'updateFromTitle']) }}
+        {{ Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'v-model' => 'title', '@keyup' => 'updateFromTitle()', '@blur' => 'updateFromTitle()']) }}
         {!! $errors->first('title', '<p class="help-block error">:message</p>') !!}
       </div>
     </div>
@@ -16,7 +16,7 @@
     <div class="form-group{!! $errors->has('slug') ? ' has-error': ' ' !!}">
       <label class="col-md-2 control-label" for="slug">Slug</label>
       <div class="col-md-10">
-        {{ Form::text('slug', null, ['class' => 'form-control', 'v-model' => 'slug', '@blur' => 'updateFromSlug']) }}
+        {{ Form::text('slug', null, ['class' => 'form-control', 'v-model' => 'slug', '@blur' => 'updateFromSlug()']) }}
         {!! $errors->first('slug', '<p class="help-block error">:message</p>') !!}
       </div>
     </div>
@@ -49,6 +49,7 @@
     data: {
       content: {!! $content->makeVisible('slug')->toJson() !!},
       sluggable: true,
+      title: '',
       slug: '',
       sidebar: {
         active: 'storycms-write'
@@ -57,11 +58,16 @@
 
     ready: function() {
       this.sluggable = ! this.$get('content.id') > 0
+      this.title = this.$get('content.title')
       this.slug = this.slugify(this.$get('content.slug') || '', '-')
     },
 
     methods: {
       slugify: function(string, separator) {
+        if (string == '') {
+          return string
+        }
+
         return string.toLowerCase()
           .replace(/^(_post_\/|_page_\/)/g, '')
           .replace(/[^\w\.]+/g, separator)
@@ -69,14 +75,19 @@
       },
 
       updateFromTitle: function() {
-        if (this.sluggable) {
-          this.slug = this.slugify(this.content.title, '-')
+        if (this.slug == '' || this.sluggable == true) {
+          this.slug = this.slugify(this.title, '-')
         }
       },
 
       updateFromSlug: function() {
+        if (this.slug == '') {
+          this.slug = this.slugify(this.title, '-')
+        } else {
+          this.slug = this.slugify(this.slug, '-')
+        }
+
         this.sluggable = false
-        this.slug = this.slugify(this.slug, '-')
       }
     }
   }).$mount('body')
